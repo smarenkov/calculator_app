92eb5371fda7f05bbec8a982d1cb71f5fba73704
Simon Marenkov <simon.marenkov@gmail.com>
2024-09-15 19:58:59 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Simon Marenkov on 15/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
